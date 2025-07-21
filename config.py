adeba20f72c3c6c2c52e304181e609e12d3088c1
hwy-10 <hwy_10@naver.com>
2025-07-21 14:25:00 +0900

    -

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 from enum import Enum
 
+stop_count = 0
+stop_threshold = 3
+
 class Status(Enum) :
     go = 0
     left = 1
@@ -21,5 +24,22 @@ PRIORITY_RULES = [
         ("both", Status.right)
     ]
 
-# back은 openCV로만 구현 -> 이후 CNN으로도 구현해야 한다면, 구현하기
-# CNN: go, left, right, stop, avoid, accel, decel 총 7개 상태
+def decide_final_status(cv_status, cnn_status): # Status class, stop_count 반환
+    # 1. stop 누적 판단 따로 처리
+    if cnn_status == Status.stop:
+        stop_count += 1
+        if stop_count >= stop_threshold:
+            return Status.stop, stop_count
+        else:
+            return Status.decelerate, stop_count # 일단 stop 시그널을 보냈을 때, 감속하는 방식으로
+
+    stop_count = 0
+
+    # 2. 우선순위 순회
+    for source, rule_status in PRIORITY_RULES:
+        if source == "cnn" and cnn_status == rule_status:
+            return rule_status, stop_count
+        if source == "both" and cnn_status == rule_status and cv_status == rule_status: # both일 때, 가중치를 두기
+            return rule_status, stop_count
+            # 굳이 같을 때 하지 않고, ex) cv = go cnn = right 라면 steering angle만 변화를 주면 될듯
+    return Status.go, stop_count # 일단 두 상태가 다른 경우에는 go라고 코드가 작성되어 있음. 추후 변경
--- main.py
+++ main.py
@@ -7,34 +7,9 @@ from enum import Enum
 from config import *
 
 status = Status.go # 초기 상태를 전진(go)로 설정
+
 afb.gpio.init() # GPIO 초기화 및 global.pi 설정
-
-stop_count = 0
-stop_threshold = 3
-
 afb.camera.init(640, 480, 30) # 카메라 초기화를 진행해줌
-
-
-def decide_final_status(cv_status, cnn_status): # Status class, stop_count 반환
-    # 1. stop 누적 판단 따로 처리
-    if cnn_status == Status.stop:
-        stop_count += 1
-        if stop_count >= stop_threshold:
-            return Status.stop, stop_count
-        else:
-            return Status.decelerate, stop_count # 일단 stop 시그널을 보냈을 때, 감속하는 방식으로
-
-    stop_count = 0
-
-    # 2. 우선순위 순회
-    for source, rule_status in PRIORITY_RULES:
-        if source == "cnn" and cnn_status == rule_status:
-            return rule_status, stop_count
-        if source == "both" and cnn_status == rule_status and cv_status == rule_status: # both일 때, 가중치를 두기
-            return rule_status, stop_count
-            # 굳이 같을 때 하지 않고, ex) cv = go cnn = right 라면 steering angle만 변화를 주면 될듯
-    return Status.go, stop_count # 일단 두 상태가 다른 경우에는 go라고 코드가 작성되어 있음. 추후 변경
-
 
 try:
     while True:
@@ -42,13 +17,9 @@ try:
         # 여기에 카메라 입력을 받아서 처리하는 코드 작성
         # openCV 인자를 받음 A가 만들어라
         # CNN 인자를 받음 B가 만들어라
-        # 메소드 def decide_final_status를 통해 status 결정하기
         cv_status =  """vision/openCV로 받는 데이터 : return speed, steering_angle"""
         cnn_status = """"vison/CNN로 받는 데이터 return speed,steering_angle, status""" 
         status, stop_count = decide_final_status(cv_status, cnn_status) # status 결정 match문에 사용
-        
-
-
 
 # 결정된 status로부터 차량을 제어하는 logic -> 구체적인 코드만 작성하면 됨
         match status : 
@@ -60,7 +31,7 @@ try:
                 motor.rear_forward(180, 45)
             case Status.right : 
                 motor.front_forward(180, 135)
-                motor.rear_forward(180, 45)
+                motor.rear_forward(180, 135)
             case Status.back :
                 motor.front_backward(180) # speed, angle = 90
                 motor.rear_backward(180) # speed, angle = 90
@@ -75,9 +46,6 @@ try:
             case Status.decelerate:
                 motor.front_forward(150) # speed down -> 속도는 이후 조정
                 motor.rear_forward(150)
-
-
-
 
 except KeyboardInterrupt: # 
     print("사용자 종료")
