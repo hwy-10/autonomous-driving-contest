e7c554f2616f2195e82926518cb14fc9a046f0bc
hwy-10 <hwy_10@naver.com>
2025-07-21 13:46:08 +0900

    -

--- main.py
+++ main.py
@@ -16,6 +16,16 @@ class Status(Enum) :
     accelerate = 6
     decelerate = 7
 
+PRIORITY_RULES = [
+        ("cnn", Status.stop),
+        ("cnn", Status.avoid),
+        ("cnn", Status.accelerate),
+        ("cnn", Status.decelerate),
+        ("both", Status.go),
+        ("both", Status.left),
+        ("both", Status.right),
+        ("cv", Status.back)
+    ]
 
 # back은 openCV로만 구현 -> 이후 CNN으로도 구현해야 한다면, 구현하기
 # CNN: go, left, right, stop, avoid, accel, decel 총 7개 상태
@@ -29,36 +39,27 @@ stop_threshold = 3
 afb.camera.init(640, 480, 30) # 카메라 초기화를 진행해줌
 
 
-def decide_final_status(cv_status, cnn_status, stop_count, stop_threshold = 3):
-    PRIORITY_RULES = [
-        ("cnn", Status.stop),
-        ("both", Status.go),
-        ("cnn", Status.avoid),
-        ("cnn", Status.left),
-        ("cnn", Status.right),
-        ("cnn", Status.accelerate),
-        ("cnn", Status.decelerate),
-        ("cv", Status.left),
-        ("cv", Status.right),
-    ]
+def decide_final_status(cv_status, cnn_status): # Status class, stop_count 반환
     # 1. stop 누적 판단 따로 처리
     if cnn_status == Status.stop:
         stop_count += 1
         if stop_count >= stop_threshold:
             return Status.stop, stop_count
         else:
-            return None, stop_count
+            return Status.decelerate, stop_count # 일단 stop 시그널을 보냈을 때, 감속하는 방식으로
+
+    stop_count = 0
 
     # 2. 우선순위 순회
     for source, rule_status in PRIORITY_RULES:
         if source == "cnn" and cnn_status == rule_status:
-            return rule_status, 0
+            return rule_status, stop_count
         if source == "cv" and cv_status == rule_status:
-            return rule_status, 0
-        if source == "both" and cnn_status == rule_status and cv_status == rule_status:
-            return rule_status, 0
+            return rule_status, stop_count
+        if source == "both" and cnn_status == rule_status and cv_status == rule_status: # both일 때, 가중치를 두기
+            return rule_status, stop_count
 
-    return Status.go, 0
+    return Status.go, stop_count
 
 
 try:
@@ -70,8 +71,7 @@ try:
         # 메소드 def decide_final_status를 통해 status 결정하기
         cv_status =  """vision/openCV로 받는 데이터 : return speed, steering_angle"""
         cnn_status = """"vison/CNN로 받는 데이터 return speed,steering_angle, status""" 
-        status, stop_count = decide_final_status(cv_status, cnn_status, stop_count) # status 결정 match문에 사용
-        # stop이 아니라면, stop count를 reset 해주는 코드를 작성해두기
+        status, stop_count = decide_final_status(cv_status, cnn_status) # status 결정 match문에 사용
         
 
 
